c5a7cde12641dbc64d42b0fbf0014282f656a5e5
Gary <gsneed@umd.edu>
2020-07-20 15:16:05 -0400

    ...

--- ctf_bot.py
+++ ctf_bot.py
@@ -7,4 +7,4 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready.')
 
-client.run('NzM0ODQxODkyMjkzODM2ODMx.XxXk9Q.qtV_C7ziK3B3yQ1rOnprJZOndHA')
+client.run('')
